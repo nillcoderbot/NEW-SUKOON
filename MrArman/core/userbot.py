9a886a17a1bedcb3cc20ae2ca2c2287474841b26
YASHU <130467694+nillcoderbot@users.noreply.github.com>
2024-03-23 14:53:04 +0530

    userbot.py

--- MrArman/core/userbot.py
+++ MrArman/core/userbot.py
@@ -54,8 +54,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("ArmanChatz")
-                await self.one.join_chat("ArmanChatz")
+                await self.one.join_chat("HINDI_ENGLISH_CHATS_CHATTING")
+                await self.one.join_chat("HINDI_ENGLISH_CHATS_CHATTING")
             except:
                 pass
             assistants.append(1)
@@ -83,8 +83,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("ArmanChatz")
-                await self.two.join_chat("ArmanChatz")
+                await self.two.join_chat("HINDI_ENGLISH_CHATS_CHATTING")
+                await self.two.join_chat("HINDI_ENGLISH_CHATS_CHATTING")
             except:
                 pass
             assistants.append(2)
@@ -112,8 +112,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("ArmanChatz")
-                await self.three.join_chat("ArmanChatz")
+                await self.three.join_chat("HINDI_ENGLISH_CHATS_CHATTING")
+                await self.three.join_chat("HINDI_ENGLISH_CHATS_CHATTING")
             except:
                 pass
             assistants.append(3)
@@ -141,8 +141,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("ArmanChatz")
-                await self.four.join_chat("ArmanChatz")
+                await self.four.join_chat("HINDI_ENGLISH_CHATS_CHATTING")
+                await self.four.join_chat("HINDI_ENGLISH_CHATS_CHATTING")
             except:
                 pass
             assistants.append(4)
